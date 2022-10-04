e292a8d5b39f03910e6de62b4c1e34668c5a1438
Michael Vaughan <mgvaughan66@gmail.com>
2022-10-04 16:35:37 -0400

    finishing Reacting to APIs lab

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+    const [films, setFilms] = useState([]);
+
+    const [people, setPeople] = useState([]);
+
+    const getFilms = () => {
+        fetch('https://ghibliapi.herokuapp.com/films')
+            .then(response => response.json())
+            .then(allFilms => setFilms(allFilms))
+    };
+
+    const getPeople = () => {
+        fetch('https://ghibliapi.herokuapp.com/people')
+            .then(response => response.json())
+            .then(allPeople => setPeople(allPeople))
+    };
+
+    return (
+        <main className="container">
+            <section className="row justify-content-evenly mt-5">
+                <button className="col-3" onClick={() => getFilms()}>Load Films</button>
+                <button className="col-3" onClick={() => getPeople()}>Load People</button>
+            </section>
+            <section className="row justify-content-evenly mt-5">
+                {films.map(film => (
+                    <div className="col-md-6" key={`film-card-${film.id}`}>
+                        <div className="card shadow my-2">
+                            <div className="cardbody">
+                                <h4 className="card-title text-center">{film.title}</h4>
+                                <p className="card-subtitle p-2">{film.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+            <section className="row justify-content-evenly mt-5">
+                {people.map(people => (
+                    <div className="col-md-4" key={`people-card-${people.id}`}>
+                        <div className="card shadow my-2">
+                            <div className="cardbody">
+                                <h4 className="cardtitle px-2 text-decoration-underline">{people.name}</h4>
+                                <h5 className="card-subtitle my-1 px-3">age: {people.age}</h5>
+                                <h5 className="card-subtitle my-1 px-3">gender: {people.gender}</h5>
+                                <h6 className="card-subtitle my-1 px 3">
+                                    <a className="my-1 px-3" href={people.url}>link to JSON bio</a>
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </main>
+    );
+};
+
+export default App;
