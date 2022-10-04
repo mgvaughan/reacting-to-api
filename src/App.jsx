ec70afbc7e245a7dca7ac0814b87b1803db26f91
Michael Vaughan <mgvaughan66@gmail.com>
2022-10-04 17:46:37 -0400

    including homepage for lab

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,27 @@ const App = () => {
             .then(allPeople => setPeople(allPeople))
     };
 
+
+    console.log('this is people')
+    console.log(people.length);
+    if (people.length === 0 && films.length === 0) {
+        return (
+            <main className="container">
+                <section className="row justify-content-evenly mt-5">
+                    <button className="col-3" onClick={() => getFilms()}>Load Films</button>
+                    <button className="col-3" onClick={() => getPeople()}>Load People</button>
+                </section>
+                <section className="row justify-content-center mt-5">
+                    <div className="col-6 card shadow my-2">
+                        <div className="cardbody">
+                            <h2 className="card-title text-center">Studio Ghibli Routing Lab</h2>
+                            <p className="card-subtitle p-2 text-center">Welcome to my covalence lab called "Reacting to APIs"!  I had a lot of fun working through this one, and am enjoying getting to learn React.</p>
+                        </div>
+                    </div>
+                </section>              
+            </main>
+        )
+    }
     return (
         <main className="container">
             <section className="row justify-content-evenly mt-5">
